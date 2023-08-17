5ddf13fa4419c6b51eb5dba20f51e0e1e3629e9a
noteshs <142491268+noteshs@users.noreply.github.com>
2023-08-17 17:20:46 +0800

    Create save.php

---
+++ save.php
@@ -0,0 +1,37 @@
+<?php
+header('Access-Control-Allow-Origin: *'); // Allow cross-origin requests
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $content = $_POST['content'];
+
+    // Create a new Gist using GitHub API
+    $data = [
+        'description' => 'Website Content',
+        'public' => true,
+        'files' => [
+            'content.txt' => [
+                'content' => $content
+            ]
+        ]
+    ];
+
+    $options = [
+        'http' => [
+            'method' => 'POST',
+            'header' => 'Content-Type: application/json',
+            'content' => json_encode($data)
+        ]
+    ];
+
+    $context = stream_context_create($options);
+    $response = file_get_contents('https://api.github.com/gists', false, $context);
+
+    if ($response) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false]);
+    }
+} else {
+    echo 'Invalid Request';
+}
+?>
